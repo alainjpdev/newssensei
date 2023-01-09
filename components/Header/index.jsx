a5f7dfe7582f7a1dac0f58d221ab16545550b2f6
Alain <ark2784@gmail.com>
2023-01-09 18:23:40 -0300

    first commit

---
+++ components/Header/index.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import {
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBIcon,
+} from 'mdbreact'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import { MDBCol } from 'mdb-react-ui-kit'
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const router = useRouter()
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <MDBNavbar className="py-0 navbarcss" expand="md">
+      <MDBNavbarToggler onClick={handleToggle} />
+      <MDBCol xs="1" className="text-center align-self-center">
+        <Link href="/">
+          <Image src="/images/news.png" width={150} height={45} alt="Logo" />
+        </Link>
+      </MDBCol>
+      <MDBCollapse id="navbarCollapse" navbar isOpen={isOpen}>
+        <MDBNavbarNav left></MDBNavbarNav>
+        <MDBNavbarNav right>
+          <MDBNavItem active={router.pathname === '/'}>
+            <Link href="/">
+              <a className="nav-link">
+                {/* <MDBIcon icon="home" className="mr-1" /> */}
+                Nuestro brief inteligente
+              </a>
+            </Link>
+          </MDBNavItem>
+          <MDBNavItem>
+            <Link href="/properties" passHref>
+              <a className="nav-link">
+                {/* <MDBIcon icon="building" className="mr-1" /> */}
+                ¿Qué hacemos?
+              </a>
+            </Link>
+          </MDBNavItem>
+          <MDBNavItem>
+            <Link href="/rent" passHref>
+              <a className="nav-link">
+                {/* <MDBIcon icon="building" className="mr-1" /> */}
+                Ayúdanos a ser más inteligentes
+              </a>
+            </Link>
+          </MDBNavItem>
+          <MDBNavItem>
+            <Link href="/mapPdf" passHref>
+              <a className="nav-link">
+                {/* <MDBIcon icon="building" className="mr-1" /> */}
+                Anúnciate con nosotros
+              </a>
+            </Link>
+          </MDBNavItem>
+          <MDBNavItem>
+            <Link href="/agents/agents" passHref>
+              <a className="nav-link">
+                {/* <MDBIcon icon="building" className="mr-1" /> */}
+                Contacto
+              </a>
+            </Link>
+          </MDBNavItem>
+        </MDBNavbarNav>
+      </MDBCollapse>
+    </MDBNavbar>
+  )
+}
+
+export default Header
---
+++ pages/index.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import Head from 'next/head'
+import Layout from '../components/Layout'
+import PropertyVip from '../components/PropertyVip'
+
+import Carousel from '../components/Carousel'
+import PropertySection from '../components/PropertySection'
+import Features from '../components/Features'
+import { propertiesMock } from '../src/constants'
+import MapboxComponent from '../components/Mapbox/Mapbox'
+import MapboxComponentTest from '../components/Mapboxtest'
+import Image from 'next/image'
+import CardSection from '../components/CardSection'
+import Header from '../components/Header'
+import Link from 'next/link'
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardText,
+  MDBCardTitle,
+  MDBCol,
+  MDBRow,
+  MDBCardImage,
+  MDBRipple,
+  MDBBtn,
+} from 'mdb-react-ui-kit'
+
+// import videoBg from "../src/videos/cut.mp4"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>News Sensei</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <div className="container-fluid p-0">
+          <div className="row">
+            <div className="col main">
+              <video
+                style={{ width: '100%', height: '100%' }}
+                muted
+                autoPlay
+                loop
+                src="/videos/intro.mp4"
+                className="main p-0 m-0 videoBg"
+                type="video/mp4"
+              />
+              <div className="content">
+                <div className="row">
+                  <div className="col maintext">
+                    Todo lo que tienes que saber,
+                    <br /> sobre todo lo que
+                    <br /> importa.trasciende. impacta.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  )
+}
